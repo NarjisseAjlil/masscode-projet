392b6ee40810a1e1335a724dc88a467f01f06a81
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-01-14 20:48:16 +0100

    add app.js and schema prisma

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+import cors from "cors";
+
+import V1Router from "./routes/v1.js";
+
+const app = express();
+
+const port = 3005;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5500"],
+  })
+);
+
+app.use("/v1", V1Router);
+
+app.use((err, req, res, next) => {
+  res.status(err.status).json({ message: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
